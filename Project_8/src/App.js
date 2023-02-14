effeea3469838e29320273790db8399fb00ab577
errunlee <32530613+errunlee@users.noreply.github.com>
2023-02-14 11:47:38 +0545

    #8 completed

---
+++ Project_8/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import Genarator from './component/Genarator';
+
+function App() {
+  return (
+   <>
+   <div className="container">
+   <div className="d-flex justify-content-center my-5">
+   <h1>Tired of Boring Old LOREM Ipsum?</h1>
+   </div>
+   <Genarator/>
+   </div>
+   
+   </>
+  );
+}
+
+export default App;
