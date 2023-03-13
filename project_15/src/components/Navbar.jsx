f56713286107c24622630dcdfe5e7ae77cc01346
errunlee <32530613+errunlee@users.noreply.github.com>
2023-03-13 18:45:29 +0545

    few refractor

--- project_15/src/components/Navbar.jsx
+++ project_15/src/components/Navbar.jsx
@@ -5,10 +5,11 @@ function Navbar() {
     <>
     <div className="fluid-container shadow d-flex justify-content-center align-items-center">
     <div className="d-flex justify-content-between align-items-center nav p-3 text-success ">
-    <div className='title'>The<strong>Cocktail</strong>DB</div>
+    <div className='title'>The<strong><i>Cocktail</i></strong>DB</div>
     <div className="links d-flex">
         <Link to='/' className='mx-2 my-0'><p className='m-0'>Home</p></Link>
         <Link to='/about' className='mx-2 my-0'><p className='m-0'>About</p></Link>
+       
     </div>
    </div>
     </div>
